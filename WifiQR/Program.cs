0a1d356750c119e177580884f5840b26c1205d9f
Jasper Meyer <nitrovent@gmail.com>
2018-12-05 21:43:46 +0100

    Add project files.

---
+++ WifiQR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WifiQR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
